4de514e02b766623cf7ed5edcc3507bfdc190f36
Maxi20fi <maxigroxx@gmail.com>
2023-08-08 22:30:27 -0300

    cambios en app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,8 @@ const app = express();
 
 const path = require("path");
 
-app.listen(3030,function(){
-    console.log("server started");
-});
+const port = process.env.PORT || 3001;
+app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`));
 
 app.get("/home",function(req,res){
     res.sendFile(path.join(__dirname,"views/home.html"));
