a18839bd615778963edfc1680e08366c390b0617
Maxi20fi <maxigroxx@gmail.com>
2023-09-01 22:55:02 -0300

    commit final

--- src/app.js
+++ src/app.js
@@ -4,24 +4,23 @@ const app = express();
 const path = require("path");
 
 const port = process.env.PORT || 3001;
-app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`));
+app.listen(port, () => console.log(`servidor corriendo en el puerto ${port}`));
 
-app.get("/home",function(req,res){
-    res.sendFile(path.join(__dirname,"views/home.html"));
+app.get("/home", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
-app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname,"views/home.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
-app.get("/registro",function(req,res){
-    res.sendFile(path.join(__dirname,"views/register.html"));
+app.get("/registro", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/register.html"));
 });
 
-app.get("/login",function(req,res){
-    res.sendFile(path.join(__dirname,"views/login.html"));
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "views/login.html"));
 });
-
 
 
 app.use(express.static("public"));
